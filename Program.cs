220809585d4bc7c17a92c24faf534570a6b9bb25
Hankey84 <hankeybanister84@gmail.com>
2023-12-24 14:05:33 +0300

    Добавил условие задачи в коментариях)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,14 @@
-﻿int ReadInt(string text)
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо 
+// задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше 
+// обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+int ReadInt(string text)
 {
     Console.Write(text);
     return Convert.ToInt32(Console.ReadLine());
@@ -24,8 +34,6 @@ void PrintArray(string[] array)
 }
 
 
-
 int size = ReadInt("Введите размер массива: ");
 string[] myArray = FillArray(size);
 PrintArray(myArray);
-;
