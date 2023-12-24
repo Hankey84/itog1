56f99c1881c82dcf9c0a0f2681698e1e13dfbffb
Hankey84 <hankeybanister84@gmail.com>
2023-12-24 13:30:53 +0300

    Создал метод для ввода с консоли и вывода на экрам массива из строк

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string[] FillArray(int size)
+{
+    string[] tempArray = new string[size];
+
+    for (int i = 0; i < size; i++)
+{
+    Console.Write($"Введите {i+1} элемент массива: ");
+    tempArray[i] = Console.ReadLine()!;
+    Console.WriteLine();
+}
+
+return tempArray;
+}
+
+void PrintArray(string[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+
+
+int size = ReadInt("Введите размер массива: ");
+string[] myArray = FillArray(size);
+PrintArray(myArray);
+;
